9569f2d9dedba6ed74b2fa8be29427c68845f5e0
Deepakch003 <157952603+Deepakch003@users.noreply.github.com>
2026-07-16 23:41:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Numbers with Given Digit Sum/numbers-with-given-digit-sum.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    int[][] dp;
+
+    public int countWays(int n, int sum) {
+
+        
+        if (sum > 9 * n || sum < 1)
+            return -1;
+
+        dp = new int[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            java.util.Arrays.fill(dp[i], -1);
+        }
+
+        int ans = 0;
+
+        
+        for (int d = 1; d <= 9; d++) {
+            if (sum >= d)
+                ans += solve(1, sum - d, n);
+        }
+
+        return ans == 0 ? -1 : ans;
+    }
+
+    private int solve(int pos, int rem, int n) {
+
+        if (rem < 0)
+            return 0;
+
+        if (pos == n)
+            return rem == 0 ? 1 : 0;
+
+        if (dp[pos][rem] != -1)
+            return dp[pos][rem];
+
+        int ways = 0;
+
+        for (int d = 0; d <= 9; d++) {
+            if (rem >= d)
+                ways += solve(pos + 1, rem - d, n);
+        }
+
+        return dp[pos][rem] = ways;
+    }
+}
